b956359b8681ebb33ec7c2c5fa96c31d9d08c56a
AnanthaBabuP <pmananthababu@gmail.com>
2024-10-06 22:32:51 +0530

    JDBC CRUD Operations

--- workspace/OopsConcepts/src/JDBC/com/files/SelectRecord.java
+++ workspace/OopsConcepts/src/JDBC/com/files/SelectRecord.java
@@ -2,23 +2,24 @@ package JDBC.com.files;
 
 import java.sql.*;
 
+import Connection_test.Connection_get;
+
 public class SelectRecord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_details","root","Ananth@1999");
-			Statement s = con.createStatement();
-			ResultSet rs = s.executeQuery("Select * FROM employee_1");
-//			while(rs.next()) {
-//				System.out.println(rs.getString(1)+ " : "+rs.getString(2)+" : "+rs.getString(3) );
-//			}
-			
-			rs = s.executeQuery("show Tables");
-			while(rs.next()) {
-				System.out.println(rs.getString(1));
+			Connection con = Connection_get.getStusent_DetailConnection();
+			if(con != null) {
+				Statement s = con.createStatement();
+				ResultSet rs = s.executeQuery("Select * FROM employee_1");
+				while(rs.next()) {
+					System.out.println(rs.getInt(1)+ " \t "+rs.getString(2)+" \t "+rs.getString(3) );
+				}
+			} else {
+				System.out.println("Connection Fail...");
 			}
+			
 			con.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
--- workspace/OopsConcepts/src/com/Inheritance/Animal.java
+++
@@ -1,9 +0,0 @@
-package com.Inheritance;
-
-public class Animal {
-	
-	String type;
-	public void sound(){
-		System.out.println("Animal Make Sound");
-	}
-}
