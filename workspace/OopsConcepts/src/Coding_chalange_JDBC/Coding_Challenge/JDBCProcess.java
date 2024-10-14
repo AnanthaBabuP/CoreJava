469af675afd3231318eb24055f7def38aa74e5a3
AnanthaBabuP <pmananthababu@gmail.com>
2024-10-14 16:31:30 +0530

    Web Application And JSTL Tags Programming Codes

--- workspace/OopsConcepts/src/Coding_chalange_JDBC/Coding_Challenge/JDBCProcess.java
+++ workspace/OopsConcepts/src/Coding_chalange_JDBC/Coding_Challenge/JDBCProcess.java
@@ -1,6 +1,7 @@
 package Coding_chalange_JDBC.Coding_Challenge;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,13 +16,15 @@ public class JDBCProcess {
 		Scanner scan = new Scanner(System.in);
 		
 		// Print the list of Process in Console
-		System.out.println("2.Displayemployeeswiththesamedesignation");
-		System.out.println("3.Findhighestsalaryemployeedetails");
-		System.out.println("4.Findemployeeswithasalarylessthanorequalto5000/-updatetheirsalaryto 15000/-");
-		System.out.println("5.Deleteemployeeswhoseageislessthan20:\n");
+		System.out.println("PROCESS DETAILS");
+		System.out.println("-----------------------");
+		System.out.println("2. DETAILS FOR SAME DESIGINATION EMPLOYEES");
+		System.out.println("3. MAX SALARY EMPLOYEE DETAIL");
+		System.out.println("4. FIND EMPLOYEE SALARY < 5000/- UPDATE SALARY = 15000/-");
+		System.out.println("5. DELETE EMPLOYEE WHOSE AGE IS LESSTHEN 20\n");
 		
 		// Get Process Number From User
-		System.out.println("Enter Process Number:");
+		System.out.print("Enter Process Number: ");
 		int process = scan.nextInt();
 		
 		
@@ -38,27 +41,30 @@ public class JDBCProcess {
 				findHighestSalaryEmployee(con);
 				break;
 			case 4:
-				System.out.println("Findemployeeswithasalarylessthanorequalto5000/-updatetheirsalaryto 15000/-");
-//				updateEmployee(con);
+				System.out.println("FIND EMPLOYEE SALARY < 5000/- UPDATE SALARY = 15000/- \n");
+				updateEmployee(con);
 				break;
 			case 5:
-				System.out.println("Deleteemployeeswhoseageislessthan20:");
-//				deleteChildEmployee(con);
+				System.out.println("DELETE EMPLOYEE WHOSE AGE IS LESSTHEN 20:\n");
+				deleteChildEmployee(con);
 				break;
 
 			default:
-				System.out.println("Invalid Process");
+				System.err.println("Invalid Process");
 				break;
 			}
-			
 		}
 		
+		// close all connections
 		scan.close();
 		con.close();
 	}
 
-	
-
+	/**
+	 *  DETAILS FOR SAME DESIGINATION EMPLOYEES:
+	 * @param con
+	 * @param scan
+	 */
 	private static void displaySameDesgination(Connection con, Scanner scan) {
 		try {
 			Statement s = con.createStatement();
@@ -76,12 +82,19 @@ public class JDBCProcess {
 									rs.getInt("AGE")+"\t"+
 									rs.getString("DESIGINATION"));
 			}
+			
+			rs.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * MAX SALARY EMPLOYEE DETAIL
+	 * @param con
+	 * @throws Exception
+	 */
 	private static void findHighestSalaryEmployee(Connection con) throws Exception {
 		Statement s = con.createStatement();
 		System.out.println("NAME \t ADDRESS \t CONTACT_NUMBER \t SALARY \t AGE \t DESIGINATION");
@@ -95,7 +108,48 @@ public class JDBCProcess {
 								rs.getInt("AGE")+"\t"+
 								rs.getString("DESIGINATION"));
 		}
-		
+		s.close();
 	}
 
+	
+	/**
+	 * FIND EMPLOYEE SALARY < 5000/- UPDATE SALARY = 15000/-
+	 * @param con
+	 */
+	private static void deleteChildEmployee(Connection con) {
+		try {
+			String query = "DELETE FROM EMPLOYEE WHERE AGE < 20";
+			PreparedStatement ps = con.prepareStatement(query);
+			int count = ps.executeUpdate();
+			if(count > 0) 
+				System.out.println(count+" Records Deleted...");
+			else
+				System.out.println("No Records Affected");
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * DELETE EMPLOYEE WHOSE AGE IS LESSTHEN 20:
+	 * @param con
+	 */
+	private static void updateEmployee(Connection con) {
+		try {
+			String query = "UPDATE EMPLOYEE SET SALARY=15000 WHERE SALARY < 5000;";
+			PreparedStatement ps = con.prepareStatement(query);
+			int count = ps.executeUpdate();
+			if(count > 0) 
+				System.out.println(count+" Records UPDATED...");
+			else
+				System.out.println("No Records Affected");
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
